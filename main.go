c5a2b05f4026cd3fc76730d927dbfa3be2b3101d
KarlMathuthu <karlmathuthu07@gmail.com>
2025-01-23 10:44:26 +0200

    modified

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main()  {
-	
+
+	app := gin.Default()
+
+	// routes.
+	app.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+		  "message": "pong",
+		})
+	  })
+
+	app.Run(":8090")
 }
